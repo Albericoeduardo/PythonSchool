5eff95dc0997c94bf6654d2092292f2fd185a841
Albérico Eduardo <115420259+Albericoeduardo@users.noreply.github.com>
2023-05-17 15:31:00 -0300

    LAUNCH code

--- Guess.py
+++ Guess.py
@@ -1,4 +1,4 @@
-import random
+'''import random
 number = random.randint(1,100)
 print(number)
 print("Jogo da adivinhação! Tente adivinhar um número entre 1 e 100:")
@@ -10,8 +10,18 @@ elif escolha >= number + 20:
     escolha = int(input("Escolha outro número: "))
 elif escolha >= number - 20:
     print("Está frio, o número está muito acima")
-    escolha = int(input("Escolha outro número: "))
+    escolha = int(input("Escolha outro número: "))'''
 
 
+import random
 
-
+number = random.randint(1, 100)
+print("Adivinhe o número de 1 a 100!!")
+escolha = int(input("Selecione um número: "))
+while True:
+    if escolha == number:
+        print(f'Acertou!! o número era {number} mesmo!')
+        break
+    else:
+        escolha = int(input("Errado! Selecione outro número: "))
+        
